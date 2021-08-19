93aaab63b2a0de02ac536dd807e8f202fb0395b0
ManApart <r@austinkucera.com>
2021-08-19 07:38:48 -0400

    auto confirm

--- MenuSkip/MainModClass.cs
+++ MenuSkip/MainModClass.cs
@@ -5,6 +5,7 @@ using StardewValley;
 using System;
 using StardewValley.Menus;
 using StardewModdingAPI.Events;
+using Microsoft.Xna.Framework;
 
 
 //https://stardewvalleywiki.com/Modding:Modder_Guide/APIs/Events
@@ -12,10 +13,12 @@ namespace MenuSkip {
 
     public class MainModClass : Mod {
         private static MainModClass instance;
+        private IClickableMenu confirmMenu = null;
 
         public override void Entry(IModHelper helper) {
            instance = this;            
             helper.Events.Input.ButtonPressed += OnMenu;
+            helper.Events.GameLoop.UpdateTicked += OnUpdate;
 
             Log("MenuSkip by Iceburg333 => Initialized");
         }
@@ -29,24 +32,40 @@ namespace MenuSkip {
                 {
                     case SButton.Space:
                     case SButton.F1:
+                    case SButton.W:
                         ChooseSelection(0, Game1.activeClickableMenu);
                         break;
                     case SButton.F2:
+                    case SButton.S:
                         ChooseSelection(1, Game1.activeClickableMenu);
                         break;
                     case SButton.F3:
+                    case SButton.A:
                         ChooseSelection(2, Game1.activeClickableMenu);
                         break;
                     case SButton.F4:
+                    case SButton.D:
                         ChooseSelection(3, Game1.activeClickableMenu);
                         break;
                     default:
                         break;
                 }
-               
 
             }
             
+        }
+
+        private void OnUpdate(object sender, EventArgs e)
+        {
+            if (confirmMenu != null && Game1.activeClickableMenu == confirmMenu)
+            {
+                confirmMenu.receiveKeyPress(Microsoft.Xna.Framework.Input.Keys.Space);
+            } else if (confirmMenu != null)
+            {
+                Game1.input.SetMousePosition(Game1.viewport.Width/2, Game1.viewport.Height/2);
+                confirmMenu = null;
+            }
+
         }
 
         private void ChooseSelection(int selection, IClickableMenu menuIn)
@@ -61,11 +80,10 @@ namespace MenuSkip {
 
                 if (menu.responses.Count > 0)
                 {
-                    Log("Picking " + menu.responses[selection].responseText);
+                    //Log("Picking " + menu.responses[selection].responseText);
                     menu.responseCC[selection].snapMouseCursorToCenter();
-                    
-                    //Point click = menu.responseCC[selection].bounds.Center;
-                    //menu.receiveLeftClick(click.X, click.Y);
+                    confirmMenu = menu;
+
                 }
 
             }
